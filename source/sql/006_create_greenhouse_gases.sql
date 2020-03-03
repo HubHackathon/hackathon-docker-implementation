cf8953643efb3b38f5629b4fc407047c97a6ecbf
todddunagan <dunagan_todd@bah.com>
2020-03-03 13:14:31 -0500

    Moving dump to zipped version

--- source/sql/006_create_greenhouse_gases.sql
+++
@@ -1,8 +0,0 @@
-create table greenhouse_gas_inventory_data_data
-(
-    country_or_area text null,
-    year int null,
-    value double null,
-    category text null
-);
-
--- source/sql/016_create_co2_emmisions.sql
+++
@@ -1,7 +0,0 @@
-create table co2_annmean_mlo_csv
-(
-    year date null,
-    mean double null,
-    uncertainty double null
-);
-
